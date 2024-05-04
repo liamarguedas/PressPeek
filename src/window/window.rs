8a3133dc9b6062ec92a964aea0d1857ed950d5ca
Liam <iliamftw2013@gmail.com>
2024-05-03 21:37:09 -0300

    Add( window size & configs )

--- src/window/window.rs
+++ src/window/window.rs
@@ -1,11 +1,20 @@
 use winit::{
     event_loop::EventLoop,
-    window:: WindowBuilder,
+    window:: {WindowBuilder, Window},
 };
 
 pub fn presspeek_window() {
+    let with: f32 = 250.0;
+    let height: f32 = 50.0;
     let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    let window: Window = WindowBuilder::new()
+        .with_title("")
+        .with_inner_size(winit::dpi::LogicalSize::new(with, height))
+        .with_decorations(true)
+        .with_always_on_top(true)
+        .build(&event_loop)
+        .unwrap();
 
     event_loop.run(move |event, _, control_flow| {
         *control_flow = winit::event_loop::ControlFlow::Wait;
